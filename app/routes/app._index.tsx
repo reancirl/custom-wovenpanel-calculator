0f23665458b71f82cc33991da38fac175ad2517e
Rey <reancirl@gmail.com>
2026-04-10 02:20:40 +0800

    Preserve Shopify query params when redirecting /app to SQM page

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,8 +1,11 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { redirect } from "react-router";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-  return redirect("/app/sqm-pricing");
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const currentUrl = new URL(request.url);
+  const queryString = currentUrl.search || "";
+
+  return redirect(`/app/sqm-pricing${queryString}`);
 };
 
 export default function AppIndexRedirect() {
